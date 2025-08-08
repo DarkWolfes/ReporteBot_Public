5797715b57f1c3fe265cae65dcf4348beda8a1c2
DarkWolfes <pablomad1989@gmail.com>
2025-08-08 17:52:23 +0200

    Delete run.sh

--- run.sh
+++
@@ -1,5 +0,0 @@
-# Instala gunicorn y uvicorn
-pip install gunicorn uvicorn
-
-# Inicia la aplicación con gunicorn y uvicorn
-gunicorn --bind 0.0.0.0:$PORT --worker-class uvicorn.workers.UvicornWorker main:app
